4202e8ad9feca42c0ac3f5ac5590a35d2a751b22
王若飞 <wangruofei@eccom.com.cn>
2021-11-04 17:43:48 +0800

    days - 2-2

--- src/api/edu/teacher.js
+++ src/api/edu/teacher.js
@@ -4,8 +4,8 @@ import request from '@/utils/request'
 export default {
     getTeacherListPage(current, limit, teacherQuery) {
         return request({
-            url: `/eduservice/teacher/pageTeacher/${current}/${limit}`,
-            method: 'get',
+            url: `/eduservice/teacher/pageTeacherCondition/${current}/${limit}`,
+            method: 'post',
             data: teacherQuery
         })
     }

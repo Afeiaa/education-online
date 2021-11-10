9765e4a6fd884ee07f9c23f6a3d9c9416cc9552f
王若飞 <wangruofei@eccom.com.cn>
2021-11-10 17:59:45 +0800

    days 9-1

--- src/views/edu/course/chapter.vue
+++ src/views/edu/course/chapter.vue
@@ -10,30 +10,80 @@
       <el-step title="最终发布"/>
     </el-steps>
 
-    <el-form label-width="120px">
+    <!-- <el-button type="text">添加章节</el-button> -->
+   <!-- 章节 -->
+    <ul class="chanpterList">
+        <li
+            v-for="chapter in chapterVideoList"
+            :key="chapter.id">
+            <p>
+                {{ chapter.title }}
 
-      <el-form-item>
+                <span class="acts">
+                    <el-button style="" type="text" @click="openVideo(chapter.id)">添加小节</el-button>
+                    <el-button style="" type="text" @click="openEditChatper(chapter.id)">编辑</el-button>
+                    <el-button type="text" @click="removeChapter(chapter.id)">删除</el-button>
+                </span>
+            </p>
+
+            <!-- 视频 -->
+            <ul class="chanpterList videoList">
+                <li
+                    v-for="video in chapter.children"
+                    :key="video.id">
+                    <p>{{ video.title }}
+
+                <span class="acts">
+                    
+                    <el-button style="" type="text">编辑</el-button>
+                    <el-button type="text" @click="removeVideo(video.id)">删除</el-button>
+                </span>
+                    </p>
+                </li>
+            </ul>
+        </li>
+    </ul>
+    <div>
         <el-button @click="previous">上一步</el-button>
         <el-button :disabled="saveBtnDisabled" type="primary" @click="next">下一步</el-button>
-      </el-form-item>
-    </el-form>
+    </div>
+
   </div>
 </template>
 
 <script>
+import chapterApi from '@/api/edu/chapter'
 
 export default {
   data() {
     return {
-      saveBtnDisabled: false // 保存按钮是否禁用
+      saveBtnDisabled: false, // 保存按钮是否禁用
+      courseId: '', // 所属课程
+      chapterVideoList: [] // 章节嵌套课时列表
     }
   },
 
   created() {
-    console.log('chapter created')
+      console.log('chapter created')
+      this.init()
   },
 
   methods: {
+    init() {
+      if (this.$route.params && this.$route.params.id) {
+        this.courseId = this.$route.params.id
+        // 根据id获取课程基本信息
+        this.getChapterVideo()
+      }
+    },
+
+    getChapterVideo() {
+      chapterApi.getAallChapterVideo(this.courseId).then(response => {
+        this.chapterVideoList = response.data.allChapterVideo
+        console.log("**************************************"+this.chapterVideoList)
+      })
+    },
+
     previous() {
       console.log('previous')
       this.$router.push({ path: '/course/info/1' })
@@ -46,3 +96,45 @@ export default {
   }
 }
 </script>
+
+
+<style scoped>
+.chanpterList{
+    position: relative;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+.chanpterList li{
+  position: relative;
+}
+.chanpterList p{
+  float: left;
+  font-size: 20px;
+  margin: 10px 0;
+  padding: 10px;
+  height: 70px;
+  line-height: 50px;
+  width: 100%;
+  border: 1px solid #DDD;
+}
+.chanpterList .acts {
+    float: right;
+    font-size: 14px;
+}
+
+.videoList{
+  padding-left: 50px;
+}
+.videoList p{
+  float: left;
+  font-size: 14px;
+  margin: 10px 0;
+  padding: 10px;
+  height: 50px;
+  line-height: 30px;
+  width: 100%;
+  border: 1px dotted #DDD;
+}
+
+</style>
